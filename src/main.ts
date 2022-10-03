3ef05fe6b8e3545e8e043eaf3d2b53085d10d39a
Henry Golding <hgolding@protonmail.com>
2022-10-03 14:07:59 +0100

    feat: make call to django api and display wine list

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
--- src/views/WineList.vue
+++ src/views/WineList.vue
@@ -1,3 +1,29 @@
 <template>
-    A list of wines.
+	<ul v-for="wine in wines" :key="wine.id">
+	<li >{{ wine.name }}</li>
+	</ul>
 </template>
+
+<script lang="ts">
+    import axios from 'axios'
+    export default {
+        name: 'WineList',
+				data() {
+					return {
+						wines: []
+					};
+				},
+				mounted() {
+					this.getWines();
+				},
+        methods: {
+            async getWines() {
+              await axios
+								.get('/api/winelist')
+								.then(response => { this.wines = response.data})
+								.catch(error => {console.log(error)})
+            }
+        },
+				
+    }
+</script>
