b7d55369dab7b3beb0f02dd4b1e00ecc023891c0
Henry Golding <hgolding@protonmail.com>
2022-10-19 20:25:36 +0100

    fix: improvement of cart details item styling

--- src/components/CartDetailsItem.vue
+++ src/components/CartDetailsItem.vue
@@ -1,33 +1,32 @@
 <template>
-  <div class="cart-item-division">
+<div class="">
+<div class="">
+  <div class="cart-item">
     <router-link :to="`details/${wine.details.id}`">
-      <div class="card-image">
+    <div class="media">
+      <div class="media-left">
         <figure class="image is-48x48">
 	  <img :src="wine.details.get_thumbnail" width="50px"/>
         </figure>
       </div>
-      <div class="class-item-info">
+      <div class="media-content">
         <h3>{{ wine.details.name }} {{ wine.details.year }}</h3>
         <p>£{{ wine.details.price_incvat }} each
           <br>{{ wine.details.stock_level }} in stock
           <br>{{ wine.quantity }} in basket
         </p>
       </div>
+    </div>
     </router-link>
-  </div>
-  <div class="cart-item-division">
-    <form action="#" method="POST">
- 
-          <input type="number" name="quantity" :id="wine.details.id + '-quantity'"
-            :value="wine.quantity" 
-            min="0" :max=wine.stock_level>
-          <a @click="updateCart(wine)" class="cart-button" type="submit" :id="wine.details.id"><i class="bi bi-cart-plus-fill"></i>update</a>
-    </form>
-    <form action="#" method="POST">
+      <input type="number" name="quantity" :id="wine.details.id + '-quantity'"
+        :value="wine.quantity" min="0" :max=wine.stock_level>
+      <a @click="updateCart(wine)" class="cart-button" type="submit" :id="wine.details.id"><i class="bi bi-cart-plus-fill"></i>update</a>
       <input type="hidden" name="item_id" value="{{ wine.details.id }}">
       <a @click="removeFromCart(wine)" class="cart-button"><i class="bi bi-x-lg" style="font-size:2rem"></i></a>
-    </form>
   </div>
+  <div class=""></div>
+</div>
+</div>
 </template>
 
 <script>
